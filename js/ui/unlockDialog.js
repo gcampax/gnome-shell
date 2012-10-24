e6b7049dd6b0fa282837b8adfa229d6741283a1b
Giovanni Campagna <gcampagna@src.gnome.org>
2012-10-24 17:53:19 +0200

    Block cancellation of the unlock dialog when dragging
    Pressing esc while the mouse is down should not make the curtain fall,
    otherwise a gray screen results.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=686800

--- js/ui/unlockDialog.js
+++ js/ui/unlockDialog.js
@@ -168,6 +168,7 @@ const UnlockDialog = new Lang.Class({
         this._promptLoginHint.hide();
         this.contentLayout.add_actor(this._promptLoginHint);
 
+        this.allowCancel = false;
         let cancelButton = { label: _("Cancel"),
                              action: Lang.bind(this, this._escape),
                              key: Clutter.KEY_Escape };
@@ -297,8 +298,10 @@ const UnlockDialog = new Lang.Class({
     },
 
     _escape: function() {
-        this._userVerifier.cancel();
-        this.emit('failed');
+        if (this.allowCancel) {
+            this._userVerifier.cancel();
+            this.emit('failed');
+        }
     },
 
     _otherUserClicked: function(button, event) {
