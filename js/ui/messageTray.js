c9c1802fa0f86898dc6be50b4215d7c7904bcd11
Giovanni Campagna <gcampagna@src.gnome.org>
2012-11-02 16:09:04 +0100

    MessageTray: pass keyboard events to tray icons
    Synthetize XKeyEvents for clicks emulated by StButton using Return or
    Space.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=687425

--- js/ui/messageTray.js
+++ js/ui/messageTray.js
@@ -1785,7 +1785,7 @@ const MessageTray = new Lang.Class({
     },
 
     _onSummaryItemClicked: function(summaryItem, button) {
-        if (summaryItem.source.handleSummaryClick()) {
+        if (summaryItem.source.handleSummaryClick(button)) {
             if (summaryItem.source.keepTrayOnSummaryClick)
                 this._setClickedSummaryItem(null);
             else
